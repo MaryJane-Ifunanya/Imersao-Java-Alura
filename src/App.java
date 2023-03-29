6317ac99dd77e37e2e5be391736351a45a9f275a
MaryJane Ifunanya <maryjaneifunanya3@gmail.com>
2023-03-29 08:01:59 -0700

    mudei alguns textos para negrito e adicionei estrelas

--- src/App.java
+++ src/App.java
@@ -26,9 +26,14 @@ public class App {
 
          // exibir e manipular os dados
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("Título: " + filme.get("title"));
-            System.out.println("Poster" + filme.get("image"));
-            System.out.println("Classificação" + filme.get("imDbRating"));
+            System.out.println("\u001b[1mTítulo: \u001b[m" + filme.get("title"));
+            System.out.println("\u001b[1mURL da imagem: \u001b[m" + filme.get("image"));
+            System.out.println("\u001b[1mClassificaçao: \u001b[m" + filme.get("imDbRating"));
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int numEstrelas = (int) classificacao;
+            for(int n =1; n <= numEstrelas; n++ ){
+                System.out.print("⭐");
+            }
             System.out.println();
         
              
